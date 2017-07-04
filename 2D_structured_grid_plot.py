0d83758f8d133bea85d12397e4f3073cfa4585b9
JamesJosephSylvester <JamesJosephSylvester@users.noreply.github.com>
2017-07-04 15:03:21 -0600

    It appears to work
    2D homogeneous and nonhomogeneous b/c with projection and Laplacian

--- 2D_structured_grid_plot.py
+++ 2D_structured_grid_plot.py
@@ -170,121 +170,139 @@ def elmt_insert(u_e, elmt):
 # Setup
 import numpy as np
 
-p_deg = 3                               # degree of shape functions
-q_deg = 10                              # degree of quadrature
-nx = 25                                 # number of elements in x
-n = nx * nx                             # number of elements
-pts_x = nx * (p_deg + 1) - (nx - 1)     # number of points in x
-pts = pts_x ** 2                        # number of points
+p_degs = range(1, 6)
+run_errs = np.zeros((max(p_degs) + 1, 10))
+run_delx = run_errs.copy()
 
-I_x = [0, 2 * np.pi]                    # interval
-I_y = [0, 2 * np.pi]                    # interval
-h = (I_x[1] - I_x[0]) / nx              # element width
+for p_deg in p_degs:
+    print(p_deg)
+    for nx in range(10, 35, 5):
+        print(nx)
+        q_deg = 10  # degree of quadrature
+        n = nx * nx  # number of elements
+        pts_x = nx * (p_deg + 1) - (nx - 1)  # number of points in x
+        pts = pts_x ** 2  # number of points
 
-def f(x, y):                            # forcing function
-    from numpy import tanh, sin, cos
-    return - 2 * sin(x) * sin(y)
+        I_x = [0, 2 * np.pi]  # interval
+        I_y = [0, 2 * np.pi]  # interval
+        h = (I_x[1] - I_x[0]) / nx  # element width
 
-def f_true(x, y):                       # true function
-    return np.sin(x) * np.sin(y) + x + y
+        u_boundary = np.zeros(pts)  # boundary
 
-# Setup Element
-B, D, q, W = element_setup()
-Dy = np.kron(D, B)
-Dx = np.kron(B, D)
-W = np.kron(W, W)
-B = np.kron(B, B)
 
-# Build J, (dX/dx)
-# 1D uniform grid
-# x = (X + 1) * h / 2 + a
-# y = (Y + 1) * h / 2 + a
-# dx/dX = h / 2
-# dy/dY = h / 2
-# |J| = h**2 / 4
-dX = 2 / h
-J = h ** 2 / 4
+        def f(x, y):  # forcing function
+            from numpy import tanh, sin, cos
+            return - 2*sin(x) * sin(y)
 
-# Boundary
-x = np.linspace(I_x[0], I_x[1], pts_x)
-poly = np.polynomial.legendre.Legendre.basis(p_deg, [-1, 1]).deriv(1)
-for i in range(nx):
-    x[i * p_deg + 1 : (i + 1) * p_deg] = (poly.roots() + 1) / dX + h * i
-X, Y = np.meshgrid(x, x)
-u_boundary = (X + Y).reshape(pts)
-u_boundary = get_bc(u_boundary)
 
-# Build Load Vector
-Mf = np.zeros(pts)
+        def f_true(x, y):  # true function
+            return np.sin(x) * np.sin(y)
 
-# Iterate
-for i in range(n):
-    row = int(np.floor(i / nx))
-    col = int(np.mod(i, nx))
 
-    x = (q + 1) / dX + h * col
-    y = (q + 1) / dX + h * row
-    X, Y = np.meshgrid(x, y)
-    f_vals = f(X, Y).reshape((q_deg ** 2))
+        # Setup Element
+        B, D, q, W = element_setup()
+        Dy = np.kron(D, B)
+        Dx = np.kron(B, D)
+        W = np.kron(W, W)
+        B = np.kron(B, B)
 
-    element_Mf = np.dot(B.T, J * W.dot(f_vals.T))
+        # Build J, (dX/dx)
+        # 1D uniform grid
+        # x = (X + 1) * h / 2 + a
+        # y = (Y + 1) * h / 2 + a
+        # dx/dX = h / 2
+        # dy/dY = h / 2
+        # |J| = h**2 / 4
+        dX = 2 / h
+        J = h ** 2 / 4
 
-    Mf += elmt_insert(element_Mf, i)
+        # Build Load Vector
+        Mf = np.zeros(pts)
 
-# Iterate to solution
-# Conjugate gradient
-u = np.random.rand(pts)
-u = np.zeros(pts)
-u = apply_bc(u)
+        # Iterate
+        for i in range(n):
+            row = int(np.floor(i / nx))
+            col = int(np.mod(i, nx))
 
-r_temp = Mf - apply_LHS(apply_bc(u))
-r_old = apply_zero_bc(r_temp) + (get_bc(u) - u_boundary)
-r_new = r_old.copy()
+            x = (q + 1) / dX + h * col
+            y = (q + 1) / dX + h * row
+            X, Y = np.meshgrid(x, y)
+            f_vals = f(X, Y).reshape((q_deg ** 2))
 
-p = r_old.copy()
-norm = 1
-itr = 1
+            element_Mf = np.dot(B.T, J * W.dot(f_vals.T))
 
-max_itr = 10*pts
+            Mf += elmt_insert(element_Mf, i)
 
-while norm > 10**-15 and itr < max_itr:
-    # Calculate new values
-    a = np.dot(r_old.T, r_old) / np.dot(p.T, apply_LHS(p))
-    u = u + a * p
+        # Iterate to solution
+        # Conjugate gradient
+        u = np.random.rand(pts)
+        u = np.zeros(pts)
+        u = apply_bc(u)
 
-    r_temp = Mf - apply_LHS(apply_bc(u))
-    r_new = apply_zero_bc(r_temp) + (get_bc(u) - u_boundary)
+        r_temp = Mf - apply_LHS(apply_bc(u))
+        r_old = apply_zero_bc(r_temp) + (get_bc(u) - u_boundary)
+        r_new = r_old.copy()
 
-    b = np.dot(r_new.T, r_new) / np.dot(r_old.T, r_old)
-    p = r_new + b * p
+        p = r_old.copy()
+        norm = 1
+        itr = 1
 
-    # Calculate error
-    norm = np.abs(np.max(r_new))
-    
-    # Early exit conditions
-    if norm > 5000:
-        itr = max_itr - 1
-    if np.max(np.abs(r_old - r_new)) < 10**-16:
-        itr = max_itr - 1
+        max_itr = 10 * pts
 
-    # Prepare for next iteration
-    itr += 1
-    r_old = r_new.copy()
+        while norm > 10 ** -15 and itr < max_itr:
+            # Calculate new values
+            a = np.dot(r_old.T, r_old) / np.dot(p.T, apply_LHS(p))
+            u = u + a * p
 
-if itr == max_itr:
-    print('max itr reached')
+            r_temp = Mf - apply_LHS(apply_bc(u))
+            r_new = apply_zero_bc(r_temp) + (get_bc(u) - u_boundary)
+
+            b = np.dot(r_new.T, r_new) / np.dot(r_old.T, r_old)
+            p = r_new + b * p
+
+            # Calculate error
+            norm = np.abs(np.max(r_new))
+
+            # Early exit conditions
+            if norm > 5000:
+                itr = max_itr - 1
+            if np.max(np.abs(r_old - r_new)) < 10**-16:
+                itr = max_itr - 1
+
+            # Prepare for next iteration
+            itr += 1
+            r_old = r_new.copy()
+
+        if itr == max_itr:
+            print('max itr reached')
+
+        x = np.linspace(I_x[0], I_x[1], pts_x)
+        poly = np.polynomial.legendre.Legendre.basis(p_deg, [-1, 1]).deriv(1)
+        for i in range(nx):
+            x[i * p_deg + 1: (i + 1) * p_deg] = (poly.roots() + 1) / dX + h * i
+        X, Y = np.meshgrid(x, x)
+
+        u_true = f_true(X, Y).reshape(pts)
+        norm = np.max(np.abs(u_true - u))
+
+        run_errs[p_deg, int(nx / 5)] = norm
+        run_delx[p_deg, int(nx / 5)] = h / p_deg
+
 
 # Plot
 import matplotlib.pyplot as plt
 
-x = np.linspace(I_x[0], I_x[1], pts_x)
-poly = np.polynomial.legendre.Legendre.basis(p_deg, [-1, 1]).deriv(1)
-for i in range(nx):
-    x[i * p_deg + 1 : (i + 1) * p_deg] = (poly.roots() + 1) / dX + h * i
-X, Y = np.meshgrid(x, x)
-plt.contourf(X, Y, u.reshape(pts_x, pts_x))
+# Plot
+symbols = iter(['o', '^', 's', '*', '>', '+'])
+for p_deg in p_degs:
+    plt.loglog(run_delx[p_deg, :], run_errs[p_deg, :], next(symbols), label = 'p = '+str(p_deg))
+    plt.loglog(run_delx[p_deg, :], run_delx[p_deg, :]**(p_deg+1), label='$h^{%d}$'%(p_deg+1))
+plt.legend()
+plt.xlabel('dx')
+plt.ylabel('||error||')
+plt.title('Error vs dx')
 plt.show()
 
-# Calculate error
-u_true = f_true(X, Y).reshape(pts)
-print('Inf Norm: ' + str(np.max(np.abs(u_true - u))))
+# Plot last result
+plt.contourf(X, Y, u.reshape(pts_x, pts_x))
+plt.show()
